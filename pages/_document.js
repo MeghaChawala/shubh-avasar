942d4f506ffeaa421e7571725b2f0726ce23141a
MeghaChawala <meghachawala21@gmail.com>
2025-08-22 22:59:21 -0400

    change title

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Shubh Avasar Boutique – Designer Ethnic Wear & Party Collection</title>
+        <meta
+          name="description"
+          content="Shubh Avasar Boutique offers exquisite ethnic wear for men and women—designer kurtas, lehengas, sharara suits and more. Book a personalized home visit today!"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
