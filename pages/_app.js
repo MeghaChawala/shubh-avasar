1fcd841ce9107101b96be3b3c648885ff211d1e4
MeghaChawala <meghachawala21@gmail.com>
2025-08-22 22:49:59 -0400

    meta pixel change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import 'swiper/css';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import Layout from "@/components/Layout";
 import { WishlistProvider } from "@/context/WishlistContext";
@@ -7,6 +9,43 @@ import { CartProvider } from "@/context/CartContext";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+
+  useEffect(() => {
+    // Load FB Pixel
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
+
+    fbq("init", "1222563596287572"); // your Pixel ID
+    fbq("track", "PageView");
+
+    // Track page changes in SPA
+    const handleRouteChange = () => {
+      fbq("track", "PageView");
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <CartProvider> {/* Wrap CartProvider outermost */}
       <WishlistProvider>
